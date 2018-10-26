3567815f64c79f3b78138348a0b4c05fc6aeeaed
fish <ycypeak@qq.com>
2018-10-26 08:12:42 +0800

    aliyun:files

---
+++ aliyun/NlsSdkCpp2.0/build/CMakeFiles/stSyncDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stSyncDemo.dir/demo/speechTranscriberSyncDemo.cpp.o"
+  "../demo/stSyncDemo.pdb"
+  "../demo/stSyncDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stSyncDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
